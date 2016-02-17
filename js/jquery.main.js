a154da93a35b4415977488bf2cb91942c446d701
Максим <max.tkh.ua@gmail.com>
2016-02-17 11:15:31 +0200

    start

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -15,37 +15,127 @@ $(function(){
         });
     });
 
-    if ($('.map').length) {
-
-        var myMap;
-
-        function init () {
-            myMap = new ymaps.Map('map', {
-                center: $('.map').attr('data-coord').split(', '),
-                zoom: 12
-            });
-            $.each($('.map'), function(i){
-                var curElem = $(this);
-
-                if (curElem.attr('data-coord')) {
-                    var coord = curElem.attr('data-coord').split(', ');
-
-                    myMap.geoObjects.add(new ymaps.Placemark(
-                        [coord[0], coord[1]],
-                        {   hintContent: "Описание",
-                            balloonContentBody: curElem.find('span').text() }, {
-                            iconLayout: 'default#image'
-                        }
-                    ));
-                }
-            });
-
-        }
-
-        ymaps.ready(init);
-    }
+    $.each($('.map'), function () {
+        new Map($(this));
+    });
 
 } );
+
+var Map = function (obj) {
+
+    //private properties
+    var _self = this,
+        _window = $(window),
+        _mapData = obj.data('map'),
+        _map = null,
+        _obj = obj;
+
+    //private methods
+    var _addEvents = function () {
+
+        },
+        _getMapData = function(){
+            _mapData.center = new google.maps.LatLng(_mapData.center[0], _mapData.center[1]);
+
+            $.each(_mapData.points, function (i) {
+                _mapData.points[i] = new google.maps.LatLng(this[0], this[1]);
+
+            });
+        },
+        _init = function () {
+            _getMapData();
+
+            google.maps.event.addDomListener(window, 'load', _initMap);
+
+            _addEvents();
+        },
+        _initMap= function(){
+
+            var MY_MAPTYPE_ID = 'custom_style',
+                featureOpts = [
+                    {
+                        featureType: "administrative",
+                        elementType: "geometry",
+                        stylers: [
+                            { saturation: -200 }
+                        ]
+                    },{
+                        featureType: "landscape",
+                        stylers: [
+                            { gamma: 0.4 },
+                            { saturation: -100 }
+                        ]
+                    },{
+                        featureType: "poi",
+                        stylers: [
+                            { saturation: -100 }
+                        ]
+                    },{
+                        featureType: "road",
+                        stylers: [
+                            { saturation: -100 }
+                        ]
+                    },{
+                        featureType: "transit",
+                        stylers: [
+                            { saturation: -100 }
+                        ]
+                    },{
+                        featureType: "water",
+                        stylers: [
+                            { hue: "#268cbc" },
+                            { gamma: 0.17 }
+                        ]
+                    }
+                ],
+                mapOptions = {
+                    zoom: _mapData.zoom,
+                    scrollwheel: false,
+                    center: _mapData.center,
+                    //disableDefaultUI: true,
+                    mapTypeControlOptions: {
+                        mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+                    },
+                    mapTypeId: MY_MAPTYPE_ID
+                },
+                styledMapOptions = {
+                    name: 'Custom Style'
+                },
+                customMapType = null,
+                image = 'img/icons-place.png';
+
+            _map = new google.maps.Map(_obj[0], mapOptions);
+
+            customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+            _map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+            $.each(_mapData.points, function () {
+                var contentString = '';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                var marker = new google.maps.Marker({
+                    position: this,
+                    map: _map,
+                    icon: image
+                });
+                marker.addListener('click', function() {
+                    infowindow.open(_map, marker);
+                });
+            });
+
+        };
+
+    //public properties
+
+    //public methods
+
+
+    _init();
+};
 
 var Menu = function (obj) {
 
---
+++ js/jquery.niceselect.js
@@ -0,0 +1,280 @@
+var selects2 = [];
+$(function(){
+
+    $( 'select' ).each( function(i){
+        selects2[i] = new AresSelect2( {
+            obj: $( this ),
+            optionType: 1,
+            showType: 2,
+            selects: selects2
+        } );
+    } );
+
+} );
+
+var AresSelect2 = function( params ){
+    this.obj = params.obj;
+    this.selects = params.selects;
+    this.optionType = params.optionType || 0;
+    this.showType = params.showType || 1;
+    this.visible = params.visible || 5;
+
+    this.init();
+};
+
+AresSelect2.prototype = {
+    init: function(){
+        var self = this;
+
+        self.core = self.core();
+        self.core.build();
+    },
+    core: function(){
+        var self = this;
+
+        return {
+            build: function(){
+                self.core.start();
+                self.core.controls();
+            },
+            start: function(){
+                self.device = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                self.text = $( '<span class="ares-select__item"></span>' );
+                self.wrap = $( '<div class="ares-select"></div>' );
+                self.cover = $( '.discount__language' );
+                self.window = $( window );
+                self.opened = false;
+
+                self.core.addWraper();
+                if( !self.optionType || self.device ){
+                    self.core.setMobileView();
+                } else if( self.optionType == 1 ){
+                    self.core.setCustom1();
+                }
+
+                self.obj[ 0 ].customSelect = this;
+            },
+            setMobileView: function(){
+                self.wrap.addClass( 'ares-select_mobile' );
+            },
+            setCustom1: function(){
+                self.wrap.addClass( 'ares-select_custom' );
+            },
+            destroy: function(){
+                self.text.remove();
+                self.wrap.unwrap();
+            },
+            addWraper: function(){
+                var curText = '';
+
+                self.obj.css( {
+                    opacity: 0
+                } );
+
+                self.obj.wrap( self.wrap );
+                self.wrap = self.obj.parent();
+                self.obj.before( self.text );
+                self.obj.find( 'option' ).each( function(){
+                    var curItem = $( this );
+
+                    if( curItem.attr( 'selected' ) == 'selected' ){
+                        curText = curItem.text();
+                    }
+                } );
+
+                if( curText == '' ){
+                    curText =  self.obj.find( 'option').eq( 0 ).text();
+                }
+                self.text.text( curText );
+            },
+            showPopup: function(){
+                var list = $( '<ul></ul>'),
+                    curScroll = self.window.scrollTop(),
+                    offset = self.wrap.offset(),
+                    maxHeight = 0,
+                    curIndex = self.obj.find( 'option:selected' ).index(),
+                    id = Math.round( Math.random() * 1000 );
+
+                if( self.opened ){
+                    self.popup.remove();
+                }
+                self.opened = true;
+
+                self.popup = $( '<div class="ares-select__popup" id="ares-select__popup' + id + '"></div>' );
+
+                if ( self.wrap.parents(".discount__selects-language").length ){
+                    self.obj.find( 'option' ).each( function(i){
+                        if ( i==0 ){
+                            var curItem = $( this );
+                            if( i == curIndex ){
+                                list.append( '<li class="active">' + curItem.text() + '</li>' );
+                            } else {
+                                list.append( '<li>' + curItem.text() + '</li>' );
+                            }
+                        } else {
+                            var curItem = $( this),
+                                src = curItem.data('src');
+                            if( i == curIndex ){
+                                list.append( '<li class="active"><img src="'+src+'">' + curItem.text() + '</li>' );
+                            } else {
+                                list.append( '<li><img src="'+src+'">' + curItem.text() + '</li>' );
+                            }
+                        }
+                    } );
+
+                } else {
+                    self.obj.find( 'option' ).each( function(i){
+                        var curItem = $( this );
+                        if( i == curIndex ){
+                            list.append( '<li class="active">' + curItem.text() + '</li>' );
+                        } else {
+                            list.append( '<li>' + curItem.text() + '</li>' );
+                        }
+                    } );
+                }
+
+                self.popup.append( list );
+                self.wrap.append( self.popup );
+
+                self.popup.css( {
+                    width: self.wrap.outerWidth(),
+                    left: 0,
+                    top: self.wrap.outerHeight()
+                } );
+
+                maxHeight = self.popup.outerHeight();
+                if( maxHeight > self.popup.find( 'li' ).eq( 0 ).outerHeight() * self.visible ){
+                    self.popup.height(self.popup.find( 'li' ).eq( 0 ).outerHeight() * self.visible);
+                    $('#ares-select__popup' + id).niceScroll({
+                        cursorcolor:"#333",
+                        cursoropacitymin: "1",
+                        cursorborderradius: "5px",
+                        cursorborder: "none",
+                        cursorwidth: "5px"
+                    });
+                }
+
+                if( self.showType == 1 ){
+                    self.popup.css( {
+                        display: 'none'
+                    } );
+                    self.popup.slideDown( 300, function(){
+                        if( self.scroll ) {
+                            self.popup.getNiceScroll().resize();
+                        }
+                    } );
+                } else if( self.showType == 2 ) {
+                    self.popup.css( {
+                        opacity: 1
+                    } );
+                    self.popup.animate( { opacity: 1 },300, function(){
+                        if( self.scroll ) {
+                            self.popup.getNiceScroll().resize();
+                        }
+                    } );
+                }
+
+                self.popup.find( 'li' ).on( {
+                    'click': function( event ){
+                        var event = event || window.event,
+                            index = $( this ).index();
+
+                        if (event.stopPropagation) {
+                            event.stopPropagation()
+                        } else {
+                            event.cancelBubble = true
+                        }
+
+                        self.obj.val( self.obj.find( 'option' ).eq( index).attr( 'value' ) );
+                        self.obj.trigger( 'change' );
+                        self.core.hidePopup();
+                        if ( self.obj.val() == "0" ){
+                            self.wrap.removeClass( 'ares-select_selected' );
+                        } else {
+                            self.wrap.addClass( 'ares-select_selected' );
+                        }
+
+                    }
+                } );
+
+            },
+
+            hidePopup: function(){
+                self.opened = false;
+                if( !self.showType ){
+                    self.popup.css( {
+                        display: 'none'
+                    } );
+                } else if( self.showType == 1 ){
+                    self.popup.stop( true, false ).slideUp( 300, function(){
+                        self.popup.remove();
+                    } );
+                } else if( self.showType == 2 ) {
+                    self.popup.stop( true, false ).fadeOut( 100, function(){
+                        self.popup.remove();
+                    } );
+                }
+                self.wrap.removeClass('active');
+            },
+            controls: function() {
+                self.obj.on( 'change', function() {
+                    if ( self.wrap.parents(".discount__selects-language").length ){
+                        self.text.text( $( this ).find( 'option:selected' ).text() );
+
+                        var txt = $( this ).find( 'option:selected' ).text(),
+                            src = null;
+                        if ( $( this ).find( 'option:selected' ).data('src') == undefined ){
+                            self.text.html(txt);
+                        } else {
+                            src = $( this ).find( 'option:selected' ).data('src');
+                            self.text.html('<img src="'+src+'">' + txt + '');
+                        }
+
+                    } else {
+                        self.text.text( $( this ).find( 'option:selected' ).text() );
+                    }
+                } );
+
+                if( self.optionType == 1 && !self.device ){
+                    self.wrap.on( {
+                        'click': function(event){
+                            var event = event || window.event;
+
+                            if (event.stopPropagation) {
+                                event.stopPropagation()
+                            } else {
+                                event.cancelBubble = true
+                            }
+
+                            $.each(self.selects, function(){
+                                if (this.obj != self.obj) {
+                                    if ( this.opened ){
+                                        this.core.hidePopup();
+                                    }
+                                }
+                            });
+
+                            if( self.opened ){
+                                self.wrap.removeClass('active');
+                                self.core.hidePopup();
+                            } else {
+                                self.wrap.addClass('active');
+                                self.core.showPopup();
+                            }
+
+                        }
+                    } );
+                    $( 'body' ).on( {
+                        'click': function(){
+                            if( self.opened ){
+                                self.wrap.removeClass('active');
+                                self.core.hidePopup();
+                            }
+                        }
+                    } );
+                }
+
+            }
+        };
+    }
+};
